f85e8b2a32b30f071e1aa5a9b466fc7b145634c1
Bushra Patel <bpatel60@yahoo.com>
2024-04-17 22:13:57 -0400

    Modifying the handleUserInput to send movement messages to the server for the respective keys in order to make the snake move in different directions

--- input.js
+++ input.js
@@ -1,11 +1,26 @@
+let connection;
+
 const handleUserInput = function(data) {
   if(data === "\u0003") {           // \u0003 maps to ctrl+c input
     process.exit();
   }
+  if(data === "\u0077" || data === "\u0057") {
+    connection.write("Move: up");
+  }
+  if(data === "\u0061" || data === "\u0041") {
+    connection.write("Move: left");
+  }
+  if(data === "\u0073" || data === "\u0053") {
+    connection.write("Move: down");
+  }
+  if(data === "\u0064" || data === "\u0044") {
+    connection.write("Move: right");
+  }
 };
 
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
