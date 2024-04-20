29eaa31cbb175ddb9934631f2e6cf901b34d0827
Bushra Patel <bpatel60@yahoo.com>
2024-04-20 19:00:13 -0400

    Using imported constant values for IP and Port

--- input.js
+++ input.js
@@ -1,34 +1,29 @@
+const { MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY, msgsObj } = require("./constants");
 let connection;
 
 const handleUserInput = function(data) {
   if(data === "\u0003") {           // \u0003 maps to ctrl+c input
     process.exit();
   }
+  
   const direction = data.toLowerCase();
   const key = data;
   
-  if(direction === 'w') {
+  if(direction === MOVE_UP_KEY) {
     connection.write("Move: up");
   }
-  if(direction === 'a') {
+  if(direction === MOVE_LEFT_KEY) {
     connection.write("Move: left");
   }
-  if(direction === 's') {
+  if(direction === MOVE_DOWN_KEY) {
     connection.write("Move: down");
   }
-  if(direction === 'd') {
+  if(direction === MOVE_RIGHT_KEY) {
     connection.write("Move: right");
   }
-
-  if(key === ',') {
-    connection.write("Say: Sssslurp!");
-  }
-  if(key === ';') {
-    connection.write("Say: I see you 0_0!");
-  }
-  if(key === '?') {
-    connection.write("Say: Where is my Treat??!!");
-  }
+  
+  connection.write(`${msgsObj[key]}`);
+  
 };
 
 // setup interface to handle user input from stdin
